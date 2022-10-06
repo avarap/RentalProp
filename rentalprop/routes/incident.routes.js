cdd956d1ef03611e2e0e9088bf63dace2e7d7d70
varaa <avarap@gmail.com>
2022-10-06 18:19:13 +0200

    Update Version

---
+++ rentalprop/routes/incident.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const User = require("../models/User.model");
+const Property = require("../models/Property.model");
+//const Incident = require("../models/Incident.model");
+
+let templatePath = "./incident";
+let redirectPath = "/incident";
+let errorRender = "error";
+
+module.exports = router;
