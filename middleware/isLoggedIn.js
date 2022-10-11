b908cbad682a70f5d03d6b08dffbb6480b559ca9
varaa <avarap@gmail.com>
2022-10-11 21:32:48 +0200

    Commit good navigation

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.user) {
-    return res.redirect("/");
+    return res.redirect("auth/index");
   }
   req.user = req.session.user;
   next();
